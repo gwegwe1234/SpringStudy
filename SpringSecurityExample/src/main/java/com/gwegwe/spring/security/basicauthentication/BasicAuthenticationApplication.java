abca41ce0153de3183a1a31bfdd3e284717b4ca8
gwegwe1234 <gwangtae5860@daum.net>
2019-01-25 10:31:07 +0900

    Spring Security

---
+++ SpringSecurityExample/src/main/java/com/gwegwe/spring/security/basicauthentication/BasicAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.gwegwe.spring.security.basicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BasicAuthenticationApplication.class, args);
+  }
+}
